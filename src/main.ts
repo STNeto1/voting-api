a6fd17a7fab9ee8a06f655ac78f78e743f849d45
STNeto1 <netofreitas7052@gmail.com>
2022-08-04 16:53:06 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ import { AppModule } from './app.module';
 import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
 
   app.useGlobalPipes(new ValidationPipe());
 
